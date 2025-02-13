bfcb7d0d098bce683bf97c34fa00e5db93dd6fdc
Markus <mstein@shivi.de>
2025-02-13 20:23:17 +0100

    Update __init__.py
    fixes #11

--- custom_components/liquid_check/__init__.py
+++ custom_components/liquid_check/__init__.py
@@ -64,6 +64,6 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     }
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        await hass.async_forward_entry_setups(entry, "sensor")
     )
     return True
